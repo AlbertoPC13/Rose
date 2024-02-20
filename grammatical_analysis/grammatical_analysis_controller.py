93af7a6a013aa7ab1e414f39439b1c3f9c6ac841
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2024-02-20 13:05:04 -0600

    Added test endpoint

--- grammatical_analysis/grammatical_analysis_controller.py
+++ grammatical_analysis/grammatical_analysis_controller.py
@@ -2,6 +2,9 @@ from flask import Blueprint, jsonify
 from .grammatical_analysis_service import GrammaticalAnalysisService
 grammatical_analysis_api = Blueprint('grammatical_analysis_api', __name__)
 
+@grammatical_analysis_api.route('/')
+def index():
+   return jsonify("Hello World"), 200
 
 @grammatical_analysis_api.route('/<text>', methods=['GET'])
 def analyze_grammar(text):
