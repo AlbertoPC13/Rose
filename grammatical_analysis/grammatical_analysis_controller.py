01b1b355860254875f8cd9ab4c9c7ff3026aa8c9
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2024-02-20 12:24:29 -0600

    Project structure modified

--- grammatical_analysis/grammatical_analysis_controller.py
+++ grammatical_analysis/grammatical_analysis_controller.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, jsonify
 from .grammatical_analysis_service import GrammaticalAnalysisService
-
 grammatical_analysis_api = Blueprint('grammatical_analysis_api', __name__)
+
 
 @grammatical_analysis_api.route('/<text>', methods=['GET'])
 def analyze_grammar(text):
