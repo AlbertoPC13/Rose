8c1a1b34352248afbdf4bba29b5a269f1300ddd6
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2024-03-11 14:04:41 -0600

    Project structure was modified to make it more modular and organized.

--- grammatical_analysis/grammatical_analysis_controller.py
+++ grammatical_analysis/grammatical_analysis_controller.py
@@ -1,13 +1,20 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, Response
+
 from .grammatical_analysis_service import GrammaticalAnalysisService
+from .natural_language_processing_tools.text_preprocessing.tokenizer.Tokenizer_nltk import TokenizerNltk
+from .natural_language_processing_tools.text_preprocessing.pos_tagger.POS_tagger_nltk import POSTaggerNltk
+from .natural_language_processing_tools.token_processor.GrammarAnalyzerOpenai import GrammarAnalyzerOpenai
+from http import HTTPStatus
+
 grammatical_analysis_api = Blueprint('grammatical_analysis_api', __name__)
 
-@grammatical_analysis_api.route('/')
-def index():
-   return jsonify("Hello World"), 200
+tokenizer = TokenizerNltk()
+pos_tagger = POSTaggerNltk()
+grammatical_analyzer = GrammarAnalyzerOpenai()
+
 
 @grammatical_analysis_api.route('/<text>', methods=['GET'])
-def analyze_grammar(text):
-    service = GrammaticalAnalysisService()
+def analyze_grammar(text: str) -> tuple[Response, int]:
+    service = GrammaticalAnalysisService(tokenizer, pos_tagger, grammatical_analyzer)
     result = service.analyze_text_grammatically(text)
-    return jsonify(result), 200
+    return jsonify(result), HTTPStatus.OK
--- grammatical_analysis/grammatical_analysis_service.py
+++ grammatical_analysis/grammatical_analysis_service.py
@@ -1,12 +1,16 @@
-from .natural_language_processing_tools.NLPNltkOpenai import NLPNltkOpenai
+from grammatical_analysis.natural_language_processing_tools.text_preprocessing.tokenizer.Tokenizer import Tokenizer
+from grammatical_analysis.natural_language_processing_tools.text_preprocessing.pos_tagger.POSTagger import POSTagger
+from grammatical_analysis.natural_language_processing_tools.token_processor.GrammarAnalyzer import GrammarAnalyzer
 
 
 class GrammaticalAnalysisService:
-    def __init__(self):
-        self.nlp = NLPNltkOpenai()
+    def __init__(self, tokenizer: Tokenizer, pos_tagger: POSTagger, grammar_analyzer: GrammarAnalyzer):
+        self.tokenizer = tokenizer
+        self.pos_tagger = pos_tagger
+        self.grammar_analyzer = grammar_analyzer
 
-    def analyze_text_grammatically(self, text_to_analyze):
-        tokenized_sentences = self.nlp.tokenize_sentences(text_to_analyze)
-        pos_tagged_sentences = self.nlp.tag_sentences_with_pos(tokenized_sentences)
-        analyzed_text = self.nlp.analyze_grammar(pos_tagged_sentences)
+    def analyze_text_grammatically(self, text_to_analyze: str) -> str:
+        tokenized_sentences = self.tokenizer.tokenize_text_by_sentences(text_to_analyze)
+        pos_tagged_sentences = self.pos_tagger.tag_sentences_with_pos(tokenized_sentences)
+        analyzed_text = self.grammar_analyzer.analyze_grammar(pos_tagged_sentences)
         return analyzed_text
