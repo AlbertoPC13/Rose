b48f5330c692a1b6ee269a6f1c7bf7c65fbae75e
Aliss55 <alicelh1600@outlook.com>
2024-03-28 18:17:09 -0600

    - dotenv helps to load the keys in .env
    - added unit test

---
+++ tests/natural_language_processing_tools/token_processor/test_GrammarAnalizerOpenai.py
@@ -0,0 +1,40 @@
+import pytest
+
+from grammatical_analysis.natural_language_processing_tools.token_processor.GrammarAnalyzerOpenai import \
+    GrammarAnalyzerOpenai
+from dotenv import load_dotenv
+
+
+class TestGrammarAnalyzerOpenai:
+
+    @pytest.fixture
+    def grammar_analyzer_openai(self):
+        return GrammarAnalyzerOpenai()
+
+    @pytest.fixture(autouse=True)
+    def load_env_variables(self):
+        load_dotenv()
+
+    # @pytest.mark.grammar_analyzer_openai
+    # def test_analyze_grammar(self, grammar_analyzer_openai):
+    #     pos_tagged_sentences = [
+    #         [("El", "DET"), ("patito", "NOUN"), ("blanco", "ADJ"), ("sonrió", "VERB"), ("feliz", "ADJ"), ("a", "ADP"), ("su", "DET"), ("nueva", "ADJ"), ("familia", "NOUN"), (",", "PUNCT"), ("pero", "CCONJ"), ("su", "DET"), ("familia", "NOUN"), ("no", "PART"), ("le", "PRON"), ("devolvió", "VERB"), ("la", "DET"), ("sonrisa", "NOUN"), (".", "PUNCT")],
+    #         [("Mamá", "NOUN"), ("Pata", "NOUN"), ("estaba", "AUX"), ("muy", "ADV"), ("confundida", "ADJ"), (".", "PUNCT")]
+    #     ]
+    #     response = grammar_analyzer_openai.analyze_grammar(pos_tagged_sentences)
+    #     expected_response = "El patito blanco sonrió feliz a su nueva familia, pero su familia no le devolvió la sonrisa.\nMamá Pata estaba muy confundida."
+    #     assert response == expected_response
+
+    @pytest.mark.grammar_analyzer_openai
+    def test_tagged_sentences_to_string(self, grammar_analyzer_openai):
+        tagged_sentences = [
+            [("El", "DET"), ("patito", "NOUN"), ("blanco", "ADJ"), ("sonrió", "VERB"), ("feliz", "ADJ"), ("a", "ADP"),
+             ("su", "DET"), ("nueva", "ADJ"), ("familia", "NOUN"), (",", "PUNCT"), ("pero", "CCONJ"), ("su", "DET"),
+             ("familia", "NOUN"), ("no", "PART"), ("le", "PRON"), ("devolvió", "VERB"), ("la", "DET"),
+             ("sonrisa", "NOUN"), (".", "PUNCT")],
+            [("Mamá", "NOUN"), ("Pata", "NOUN"), ("estaba", "AUX"), ("muy", "ADV"), ("confundida", "ADJ"),
+             (".", "PUNCT")]
+        ]
+        response = grammar_analyzer_openai.tagged_sentences_to_string(tagged_sentences)
+        expected_response = "El/DET patito/NOUN blanco/ADJ sonrió/VERB feliz/ADJ a/ADP su/DET nueva/ADJ familia/NOUN ,/PUNCT pero/CCONJ su/DET familia/NOUN no/PART le/PRON devolvió/VERB la/DET sonrisa/NOUN ./PUNCT\nMamá/NOUN Pata/NOUN estaba/AUX muy/ADV confundida/ADJ ./PUNCT"
+        assert response == expected_response
