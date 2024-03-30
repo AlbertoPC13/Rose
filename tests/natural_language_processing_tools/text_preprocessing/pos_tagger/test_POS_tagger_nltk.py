1fcaa06e3e54c222d3f3c2e2fb2e6bcc810ec762
Mdperez19 <mdperezpriego@gmail.com>
2024-03-30 14:09:41 -0600

    Add trained_tagger_from_file fixture to TestPOSTaggerNltk

--- tests/natural_language_processing_tools/text_preprocessing/pos_tagger/test_POS_tagger_nltk.py
+++ tests/natural_language_processing_tools/text_preprocessing/pos_tagger/test_POS_tagger_nltk.py
@@ -2,14 +2,21 @@ import pytest
 
 from grammatical_analysis.natural_language_processing_tools.text_preprocessing.pos_tagger.POS_tagger_nltk import \
     POSTaggerNltk
+from grammatical_analysis.natural_language_processing_tools.text_preprocessing.pos_tagger.tagger_file.TaggerFile import \
+    TaggerFile
 
 
 class TestPOSTaggerNltk:
 
     @pytest.fixture
-    def pos_tagger_nltk(self):
-        return POSTaggerNltk()
+    def trained_tagger_from_file(self):
+        return TaggerFile.load_tagger('../../../../grammatical_analysis/natural_language_processing_tools'
+                                      '/text_preprocessing/pos_tagger/tagger_file'
+                                      '/universal_tagger_default_regex_sents.pkl')
 
+    @pytest.fixture
+    def pos_tagger_nltk(self, trained_tagger_from_file):
+        return POSTaggerNltk(trained_tagger_from_file)
 
     @pytest.mark.pos_tagger_nltk
     def test_tag_sentences_with_pos(self, pos_tagger_nltk):
@@ -19,4 +26,4 @@ class TestPOSTaggerNltk:
         tagged_sentences = pos_tagger_nltk.tag_sentences_with_pos(sentences)
         # Then
         assert isinstance(tagged_sentences, list) and len(tagged_sentences) > 0
-        assert isinstance(tagged_sentences[0][0],tuple)
+        assert isinstance(tagged_sentences[0][0], tuple)
