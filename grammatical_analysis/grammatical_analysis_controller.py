2dec01294c53995c069022fbf9f32083c09ce538
Mdperez19 <mdperezpriego@gmail.com>
2024-03-30 14:09:34 -0600

    Add trained tagger from file for POS tagging

--- grammatical_analysis/grammatical_analysis_controller.py
+++ grammatical_analysis/grammatical_analysis_controller.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, jsonify, Response
 from .grammatical_analysis_service import GrammaticalAnalysisService
+from .natural_language_processing_tools.text_preprocessing.pos_tagger.tagger_file.TaggerFile import TaggerFile
 from .natural_language_processing_tools.text_preprocessing.tokenizer.Tokenizer_nltk import TokenizerNltk
 from .natural_language_processing_tools.text_preprocessing.pos_tagger.POS_tagger_nltk import POSTaggerNltk
 from .natural_language_processing_tools.token_processor.GrammarAnalyzerOpenai import GrammarAnalyzerOpenai
 from http import HTTPStatus
 
 grammatical_analysis_api = Blueprint('grammatical_analysis_api', __name__)
-
+trained_tagger_from_file = TaggerFile.load_tagger('grammatical_analysis/natural_language_processing_tools'
+                                                  '/text_preprocessing/pos_tagger/tagger_file/'
+                                                  'universal_tagger_default_regex_sents.pkl')
 tokenizer = TokenizerNltk()
-pos_tagger = POSTaggerNltk()
+pos_tagger = POSTaggerNltk(trained_tagger_from_file)
 grammatical_analyzer = GrammarAnalyzerOpenai()
 
 
