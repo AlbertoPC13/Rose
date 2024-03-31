af5a50fe161a9cef3961f32989a7326655771241
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2024-03-30 23:12:10 -0600

    Prompt is now get by an external JSON file

--- grammatical_analysis/natural_language_processing_tools/token_processor/GrammarAnalyzerOpenai.py
+++ grammatical_analysis/natural_language_processing_tools/token_processor/GrammarAnalyzerOpenai.py
@@ -7,21 +7,25 @@ class GrammarAnalyzerOpenai(GrammarAnalyzer):
     def __init__(self):
         openai_key = os.environ.get('OPENAI_API_KEY')
         self.client = OpenAI(api_key=openai_key)
+        with (open("grammatical_analysis/natural_language_processing_tools/token_processor/prompt.json", "r")
+              as prompt_file):
+            self.prompt_json = eval(prompt_file.read())
 
     def analyze_grammar(self, pos_tagged_sentences: list) -> str:
-        text_to_analyze = tagged_sentences_to_string(pos_tagged_sentences)
+        text_to_analyze = self.tagged_sentences_to_string(pos_tagged_sentences)
         completion = self.client.chat.completions.create(
             model="gpt-4",
             messages=[
                 {
-                    "role": "system",
-                    "content": "Analiza la oración que el usuario proporcionará con etiquetas POS en términos de su estructura gramatical. El análisis debe estar adaptado al nivel A1 en español mexicano, identificando posibles errores sintácticos o semánticos. Proporciona una explicación sencilla en español, como si estuvieras enseñando a un estudiante de este nivel. El resultado hazlo en JSON",
-                },
+                    "role": self.prompt_json["role"],
+                    "content": self.prompt_json["content"]
+                }
+                ,
                 {
                     "role": "user",
                     "content": text_to_analyze,
-                },
-            ],
+                }
+            ]
         )
 
         response = completion.choices[0].message.content
