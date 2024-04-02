ac1fbd2e6f44740c7a0267e327d4556f66539a55
Alberto Palacios Cabrera <BetucaPC@hotmail.com>
2024-04-01 23:48:24 -0600

    Refactored the controller and service to use the new Tokenizer (word & sentence) classes

--- grammatical_analysis/grammatical_analysis_controller.py
+++ grammatical_analysis/grammatical_analysis_controller.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, Response, request
 from .grammatical_analysis_service import GrammaticalAnalysisService
 from .natural_language_processing_tools.text_preprocessing.pos_tagger.tagger_file.TaggerFile import TaggerFile
-from .natural_language_processing_tools.text_preprocessing.tokenizer.Tokenizer_nltk import TokenizerNltk
+from .natural_language_processing_tools.text_preprocessing.tokenizer.sentence.SentenceTokenizerNltk import SentenceTokenizerNltk
+from .natural_language_processing_tools.text_preprocessing.tokenizer.word.WordTokenizerNltk import WordTokenizerNltk
 from .natural_language_processing_tools.text_preprocessing.pos_tagger.POS_tagger_nltk import POSTaggerNltk
 from .natural_language_processing_tools.token_processor.GrammarAnalyzerOpenai import GrammarAnalyzerOpenai
 from http import HTTPStatus
@@ -10,7 +11,8 @@ grammatical_analysis_api = Blueprint('grammatical_analysis_api', __name__)
 trained_tagger_from_file = TaggerFile.load_tagger('grammatical_analysis/natural_language_processing_tools'
                                                   '/text_preprocessing/pos_tagger/tagger_file/'
                                                   'universal_tagger_default_regex_sents.pkl')
-tokenizer = TokenizerNltk()
+sentence_tokenizer = SentenceTokenizerNltk()
+word_tokenizer = WordTokenizerNltk()
 pos_tagger = POSTaggerNltk(trained_tagger_from_file)
 grammatical_analyzer = GrammarAnalyzerOpenai()
 
@@ -21,7 +23,7 @@ def analyze_grammar() -> tuple[Response, int]:
         data = request.get_json()
         text = data.get('text')
         if text:
-            service = GrammaticalAnalysisService(tokenizer, pos_tagger, grammatical_analyzer)
+            service = GrammaticalAnalysisService(sentence_tokenizer, word_tokenizer, pos_tagger, grammatical_analyzer)
             result = service.analyze_text_grammatically(text)
             return jsonify(result), HTTPStatus.OK
         else:
