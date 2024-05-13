0cb1c73e8335ddb9a4d353f8767ccf8f3d98d7eb
Mdperez19 <mdperezpriego@gmail.com>
2024-05-12 18:21:30 -0600

    Added CORS configuration

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+
 from grammatical_analysis.grammatical_analysis_controller import grammatical_analysis_api
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/grammatical_analysis/*": {"origins": "*"}})
 
 if os.path.exists('.env'):
     load_dotenv()
